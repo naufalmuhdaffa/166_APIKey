487168b51c6f56aa25a0d7683b133a92c7359cd9
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-11 15:48:49 +0700

    Connect database

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
 const crypto = require('crypto');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  port: process.env.DB_PORT
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
